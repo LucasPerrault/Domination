5e3a137d4e89cb24e61b15beafb5a5de82aa0661
Augustin Simon <augussim@gmail.com>
2018-12-03 16:32:52 +0100

    add [BACK] Generation aléatoire + csv

--- src/DOMINO.java
+++ src/DOMINO.java
@@ -3,6 +3,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 import java.util.stream.Stream;
 
 
@@ -28,6 +30,12 @@ public static void main(String[] args) {
 				listeDesDominos.add(dominos);
 			});
 
+		//Set cles = couleurParJoueur.KeySet;
+		}
+			
+			
+			
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
