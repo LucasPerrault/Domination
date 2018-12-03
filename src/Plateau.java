957636151131d113cfa563e03dcbe000f9d084cf
LucasPerrault <lucas.perrault@hotmail.com>
2018-12-03 14:53:34 +0100

    ADD [BACK] nouvelle classe plateau

---
+++ src/Plateau.java
@@ -0,0 +1,10 @@
+
+public class Plateau {
+	double largeur, longueur, nbMaxDomino;
+	
+	public Plateau(double largeurInit, double longueurInit, double nbMaxDominoInit) {
+		this.largeur = largeurInit;
+		this.longueur = longueurInit;
+		this.nbMaxDomino = nbMaxDominoInit;
+	}
+}
