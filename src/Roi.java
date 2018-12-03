75aac26a1dd7f59cd1a1a4f77ca5e3be968c81bb
Perrault <lucas.perrault@hotmail.com>
2018-12-03 16:17:46 +0100

    Modif roi
    Modif roi

--- src/Roi.java
+++ src/Roi.java
@@ -8,4 +8,47 @@ public class Roi {
 		this.positionX = positionXInit;
 		this.positionY = positionYInit;
 	}
+	
+	/**
+	 * Permet de retourner la couleur du roi
+	 * @return couleur
+	 */
+	private String getCouleur() {
+		return this.couleur;
+	}
+	
+	
+	/**
+	 * Permet de retourner la position du roi sur l'axe des abcisses
+	 * @return position en X du roi
+	 */
+	private double getPositionX() {
+		return this.positionX;
+	}
+	
+	/**
+	 * Permet de retourner la position du roi sur l'axe des ordonnées
+	 * @return position en Y du roi
+	 */
+	private double getPositionY() {
+		return this.positionY;
+	}
+	
+	/**
+	 * Permet de modifier la position en abcisse du roi
+	 * @param position
+	 */
+	private void setPositionX(double position) {
+		this.positionX = position;
+	}
+	
+	/**
+	 * Permet de modifier la position en ordonnée du roi
+	 * @param position
+	 */
+	private void setPositionY(double position) {
+		this.positionY = position;
+	}
+	
+	
 }
