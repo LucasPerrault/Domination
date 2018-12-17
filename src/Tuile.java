06768047544e6c0c16b480508253689175c9a323
Perrault <lucas.perrault@hotmail.com>
2018-12-17 14:16:53 +0100

    Ajout de l'objet Tuile

---
+++ src/Tuile.java
@@ -0,0 +1,63 @@
+/**
+ * Une tuile représente la moitié d'un Domino. Elle est composée d'un nombre de couronne et d'un type
+ */
+public class Tuile {
+	
+	private int nbCouronne;
+	private String type;
+	private int positionX, positionY;
+	
+	public Tuile(String typeInit, int nbCouronneInit) {
+		this.nbCouronne = nbCouronneInit;
+		this.type = typeInit;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cette tuile est de type" + this.type + "et contient " + this.nbCouronne + "\n";
+	}
+	
+	/*
+	 * Affiche le nom de couronne
+	 * @return nbCouronne
+	 */
+	public int getNbCouronne() {
+		return this.nbCouronne;
+	}
+	
+	/**
+	 * Affiche le type de la tuile
+	 * @return
+	 */
+	public String getType() {
+		return this.type;
+	}
+	
+	/**
+	 * Retourne la position X 
+	 * @return position x
+	 */
+	public int getPositionX() {
+		return this.positionX;
+	}
+	
+	/**
+	 * Retourne position y
+	 * @return position y
+	 */
+	public int getPositionY() {
+		return this.positionY;
+	}
+	
+	/**
+	 * Définit une nouvelle position x
+	 * @param positionX
+	 */
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+	
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+}
