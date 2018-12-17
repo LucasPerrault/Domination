705aa1ce56dccdda496843e666c5c580850c2d37
Perrault <lucas.perrault@hotmail.com>
2018-12-17 14:18:33 +0100

    Modification de la classe des dominos suite à la création de la classe tuile

--- src/Domino.java
+++ src/Domino.java
@@ -1,61 +1,29 @@
-
+/**
+ * Un domino est composé de deux tuiles sur une face et d'un numéro sur l'autre face.
+ */
 public class Domino {
 	
-	private int numero, nbCouronneRight, nbCouronneLeft;
-	private String typeRight, typeLeft;
+	private int numero;
+	private Tuile tuileRight, tuileLeft;
 
-	/*
-	 * Constructeur de Domino 
-	 * @param nbCouronneRightInit
-	 * @param nbCouronneLeftInit
+	/**
+	 * Constructeur de Domino
 	 * @param numeroInit
-	 * @param typeRightInit
-	 * @param typeLeftInit
+	 * @param tuileLeftInit
+	 * @param tuileRightInit
 	 */
-	public Domino(int nbCouronneLeftInit, int nbCouronneRightInit, int numeroInit, String typeLeftInit, String typeRightInit) {
-		this.nbCouronneRight = nbCouronneRightInit;
-		this.nbCouronneLeft = nbCouronneLeftInit;
+	public Domino(int numeroInit, Tuile tuileLeftInit, Tuile tuileRightInit) {
 		this.numero = numeroInit;
-		this.typeRight = typeRightInit;
-		this.typeLeft = typeLeftInit;
+		this.tuileLeft = tuileLeftInit;
+		this.tuileRight = tuileRightInit;
 	}
 	
 	@Override
 	public String toString() {
-		return "Dominos [nbCouronneRight=" + nbCouronneRight + ", nbCouronneLeft=" + nbCouronneLeft + ", numero="
-				+ numero + ", typeLeft=" + typeLeft + ", typeRight=" + typeRight + "]";
-	}
-
-	/*
-	 * Affiche le nom de couronne à droite
-	 * @return nbCouronneRight
-	 */
-	public int getNbCouronneRight() {
-		return this.nbCouronneRight;
-	}
-
-	/*
-	 * Modifie le nombre de couronne à droite 
-	 * @param nbCouronneRight
-	 */
-	public void setNbCouronneRight(int nbCouronneRight) {
-		this.nbCouronneRight = nbCouronneRight;
-	}
-
-	/*
-	 * Affichage le nombre de courone à gauche 
-	 * @return nbCouronneLeft
-	 */
-	public int getNbCouronneLeft() {
-		return this.nbCouronneLeft;
-	}
-
-	/*
-	 * Modifie le nombre de courone à gauche 
-	 * @param nbCouronneLeft
-	 */
-	public void setNbCouronneLeft(int nbCouronneLeft) {
-		this.nbCouronneLeft = nbCouronneLeft;
+		return 	"Le domino est le numéro " + 
+				this.numero + "ayant comme tuile: " + 
+				this.tuileLeft.toString() + 
+				"et" + this.tuileRight.toString();
 	}
 
 	/*
@@ -65,46 +33,5 @@ public class Domino {
 	public int getNumeroDomino() {
 		return this.numero;
 	}
-
-	/**
-	 * Ajoute un numero de Domino
-	 * @param numeroDomino
-	 */
-	public void setNumeroDomino(int numero) {
-		this.numero = numero;
-	}
-
-	/*
-	 * Affichage le type de carte à droite du domino
-	 * @return typeRight
-	 */
-	public String getTypeRight() {
-		return this.typeRight;
-	}
-
-	/*
-	 * Ajoute le type de carte à droite du domino 
-	 * @param type1
-	 */
-	public void setTypeRight(String typeRight) {
-		this.typeRight = typeRight;
-	}
-
-	/*
-	 * Affiche le type de carte à gauche 
-	 * @return typeLeft
-	 */
-	public String getTypeLeft() {
-		return this.typeLeft;
-	}
-
-	/*
-	 * Ajoute le type de carte à gauche du domino 
-	 * @param typeLeft
-	 */
-	public void setType2(String typeLeft) {
-		this.typeLeft = typeLeft;
-	}
-	
 	
 }
